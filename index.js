e02d04ee0e040687b3c2204b2f40f5c4ebc16a49
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-12-05 07:18:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ static sanitize(string){
     return string.replace(/[^-,'A-Za-z0-9\s]+/g, '');
 }
 static titleize(string){
- //takes in a string and capitalizes all words in a sentence except the, a, an, but, of, and, for, at, by, and from; and always capitalizes the first word.
+ //takes in a string and capitalizes all words in a sentence //except the, a, an, but, of, and, for, at, by, and from; and //always capitalizes the first word.
     const ex = ['the','a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
     
     let a = string.split(' ');
    
-    let last = a.map(function(word){
+    let lastString = a.map(function(word){
       let isTrue = ex.includes(word);
     
         if(!isTrue){
